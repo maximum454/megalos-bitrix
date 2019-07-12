1c040045e7fd0678e224a899b6882856830e3d1a
maxim <maximum454@gmail.com>
2019-07-12 11:02:41 +0600

    main-company

--- index.php
+++ index.php
@@ -2,7 +2,74 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Компания Alta Group");
 ?>
-<?$APPLICATION->IncludeComponent("bitrix:news.list", "slider", Array(
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.list",
+	"slider",
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "N",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "N",
+		"DISPLAY_DATE" => "N",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "3",
+		"IBLOCK_TYPE" => "slider",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "81",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Слайдер",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "350",
+		"PROPERTY_CODE" => array(
+			0 => "MARK",
+			1 => "mark",
+			2 => "",
+		),
+		"SET_BROWSER_TITLE" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "N",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "N",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => "slider"
+	),
+	false
+);?>
+
+<?$APPLICATION->IncludeComponent("bitrix:news.list", "main-company", Array(
 	"ACTIVE_DATE_FORMAT" => "d.m.Y",	// Формат показа даты
 		"ADD_SECTIONS_CHAIN" => "N",	// Включать раздел в цепочку навигации
 		"AJAX_MODE" => "N",	// Включить режим AJAX
@@ -20,7 +87,7 @@ $APPLICATION->SetTitle("Компания Alta Group");
 		"DISPLAY_DATE" => "N",	// Выводить дату элемента
 		"DISPLAY_NAME" => "Y",	// Выводить название элемента
 		"DISPLAY_PICTURE" => "Y",	// Выводить изображение для анонса
-		"DISPLAY_PREVIEW_TEXT" => "Y",	// Выводить текст анонса
+		"DISPLAY_PREVIEW_TEXT" => "N",	// Выводить текст анонса
 		"DISPLAY_TOP_PAGER" => "N",	// Выводить над списком
 		"FIELD_CODE" => array(	// Поля
 			0 => "",
@@ -28,30 +95,30 @@ $APPLICATION->SetTitle("Компания Alta Group");
 		),
 		"FILTER_NAME" => "",	// Фильтр
 		"HIDE_LINK_WHEN_NO_DETAIL" => "N",	// Скрывать ссылку, если нет детального описания
-		"IBLOCK_ID" => "3",	// Код информационного блока
-		"IBLOCK_TYPE" => "slider",	// Тип информационного блока (используется только для проверки)
+		"IBLOCK_ID" => "5",	// Код информационного блока
+		"IBLOCK_TYPE" => "alta_group",	// Тип информационного блока (используется только для проверки)
 		"INCLUDE_IBLOCK_INTO_CHAIN" => "N",	// Включать инфоблок в цепочку навигации
 		"INCLUDE_SUBSECTIONS" => "Y",	// Показывать элементы подразделов раздела
 		"MESSAGE_404" => "",	// Сообщение для показа (по умолчанию из компонента)
-		"NEWS_COUNT" => "81",	// Количество новостей на странице
+		"NEWS_COUNT" => "5",	// Количество новостей на странице
 		"PAGER_BASE_LINK_ENABLE" => "N",	// Включить обработку ссылок
 		"PAGER_DESC_NUMBERING" => "N",	// Использовать обратную навигацию
 		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",	// Время кеширования страниц для обратной навигации
 		"PAGER_SHOW_ALL" => "N",	// Показывать ссылку "Все"
 		"PAGER_SHOW_ALWAYS" => "N",	// Выводить всегда
 		"PAGER_TEMPLATE" => ".default",	// Шаблон постраничной навигации
-		"PAGER_TITLE" => "Слайдер",	// Название категорий
+		"PAGER_TITLE" => "Компания ALta Group",	// Название категорий
 		"PARENT_SECTION" => "",	// ID раздела
 		"PARENT_SECTION_CODE" => "",	// Код раздела
-		"PREVIEW_TRUNCATE_LEN" => "350",	// Максимальная длина анонса для вывода (только для типа текст)
+		"PREVIEW_TRUNCATE_LEN" => "",	// Максимальная длина анонса для вывода (только для типа текст)
 		"PROPERTY_CODE" => array(	// Свойства
-			0 => "mark",
+			0 => "",
 			1 => "",
 		),
 		"SET_BROWSER_TITLE" => "N",	// Устанавливать заголовок окна браузера
 		"SET_LAST_MODIFIED" => "N",	// Устанавливать в заголовках ответа время модификации страницы
 		"SET_META_DESCRIPTION" => "N",	// Устанавливать описание страницы
-		"SET_META_KEYWORDS" => "Y",	// Устанавливать ключевые слова страницы
+		"SET_META_KEYWORDS" => "N",	// Устанавливать ключевые слова страницы
 		"SET_STATUS_404" => "N",	// Устанавливать статус 404
 		"SET_TITLE" => "N",	// Устанавливать заголовок страницы
 		"SHOW_404" => "N",	// Показ специальной страницы
@@ -64,71 +131,6 @@ $APPLICATION->SetTitle("Компания Alta Group");
 	false
 );?>
 
-
-        <section class="main-company">
-            <header class="main-company__header">
-                <h2 class="main-company__title">Компания Alta Group</h2>
-            </header>
-            <div class="main-company__main">
-                <article class="main-company__item">
-                    <a href="#" class="main-company__img">
-                        <img src="<?=SITE_TEMPLATE_PATH;?>/img//main-company-1.svg" alt="">
-
-                    </a>
-                    <a class="main-company__link" href="#">
-                        <h3 class="main-company__name">УСобственное производство и склад</h3>
-                    </a>
-                </article>
-                <article class="main-company__item">
-                    <a href="#" class="main-company__img">
-                        <img src="<?=SITE_TEMPLATE_PATH;?>/img//main-company-2.svg" alt="">
-
-                    </a>
-                    <a class="main-company__link" href="#">
-                        <h3 class="main-company__name">Научно- исследовательская база и лаборатория</h3>
-                    </a>
-                </article>
-                <article class="main-company__item">
-                    <a href="#" class="main-company__img">
-                        <img src="<?=SITE_TEMPLATE_PATH;?>/img//main-company-3.svg" alt="">
-
-                    </a>
-                    <a class="main-company__link" href="#">
-                        <h3 class="main-company__name">Собственные
-                            запатентованные
-                            разработки</h3>
-                    </a>
-                </article>
-                <article class="main-company__item">
-                    <a href="#" class="main-company__img">
-                        <img src="<?=SITE_TEMPLATE_PATH;?>/img//main-company-4.svg" alt="">
-
-                    </a>
-                    <a class="main-company__link" href="#">
-                        <h3 class="main-company__name">Программы
-                            с ведущими
-                            вузами отрасли</h3>
-                    </a>
-                </article>
-                <article class="main-company__item">
-                    <a href="#" class="main-company__img">
-                        <img src="<?=SITE_TEMPLATE_PATH;?>/img//main-company-5.svg" alt="">
-
-                    </a>
-                    <a class="main-company__link" href="#">
-                        <h3 class="main-company__name">Широкая
-                            дилерская
-                            сеть</h3>
-                    </a>
-                </article>
-            </div>
-            <footer class="main-company__footer">
-                <p>Компания Alta Group более 10 лет предлагает комплексный подход к водоочистке, являясь российским
-                    производителем очистных сооружений для хозяйственно-бытовых, поверхностных и производственных
-                    сточных вод, а также продукции для наружных инженерных сетей.</p>
-                <a class="main-company__btn" href="#">Подробнее о компании</a>
-            </footer>
-        </section>
 
         <section class="main-products">
             <header class="main-products__header">
